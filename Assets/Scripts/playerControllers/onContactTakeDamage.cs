67d1fc555202e5c73dbcc9fa4cbf3619753561d0
DavisLahm <doctordoctorhzg@gmail.com>
2019-01-29 09:35:03 -0800

    shotgunChanges1

---
+++ Assets/Scripts/playerControllers/onContactTakeDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onContactTakeDamage : MonoBehaviour
+{
+    public damageType myTyping;
+    public float damage = 5f;
+    public void OnCollisionEnter(Collision collision)
+    {
+        m_Health contacthealth = collision.gameObject.GetComponent<m_Health>();
+        if(contacthealth != null)
+        {
+            contacthealth.takeDamage(damage, myTyping);
+        }
+        
+
+        
+    }
+}

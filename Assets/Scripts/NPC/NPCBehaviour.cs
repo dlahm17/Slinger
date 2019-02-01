1bd1b9d566a188f94301ec2756be019b7206e826
DavisLahm <doctordoctorhzg@gmail.com>
2019-02-01 08:21:35 -0800

    NPCBehaviours

--- Assets/Scripts/NPC/NPCBehaviour.cs
+++ Assets/Scripts/NPC/NPCBehaviour.cs
@@ -1,8 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPCBehaviour : MonoBehaviour
 {
-    
+    NavMeshAgent myMovement;
+    GameObject NavMeshLocations;
+
+
+
+    private void Start()
+    {
+        myMovement = GetComponent<NavMeshAgent>();
+        NavMeshLocations = GameObject.Find("NavMeshHelper");
+
+    }
+
+    private void Update()
+    {
+
+    }
 }
